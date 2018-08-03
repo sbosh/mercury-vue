5ce3908419a6ce333169f7ba6219239a6582f0da
mitkovalkov <mitkovalkov@mail.bg>
2018-08-03 14:42:20 +0300

    test

--- src/components/buildings/BuildingInnerComponent.vue
+++ src/components/buildings/BuildingInnerComponent.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="inner-building">
+<div class="inner-building">
+  <transition name="slide-fade">
+    <div>
     <h3>Inner Building</h3>
-  </div>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam amet quam placeat odit molestias est quos, iure sed excepturi delectus, necessitatibus, exercitationem dignissimos. Magni hic consequuntur asperiores modi nostrum minus.</p>
+    <img src="@/assets/images/building01.jpg" alt="">
+    </div>
+  </transition>
+</div>
 </template>
 
 <script>
@@ -9,3 +15,22 @@ export default {
   name: 'building-inner'
 }
 </script>
+
+<style>
+img {
+  max-width: 500px;
+  margin: 50px auto;
+  display: block;
+}
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
