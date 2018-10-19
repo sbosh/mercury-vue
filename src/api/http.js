d610e5766310db02d9f841f3de63e36bea4cc68e
mitkovalkov <mitkovalkov@mail.bg>
2018-10-19 21:28:11 +0300

    remove timeout

--- src/api/http.js
+++ src/api/http.js
@@ -3,7 +3,6 @@ export default (() => {
   const instance = axios.create({
     baseURL: 'http://mercury-api.imigix.com/api',
     withCredentials: false,
-    timeout: 5000,
     headers: {
       'Accept': 'application/json',
       'Access-Control-Allow-Origin': '*',
